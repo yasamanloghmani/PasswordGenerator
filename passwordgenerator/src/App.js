60a284a99a3c4fa6ea2c0baecbfa6cfa7098d0e6
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-05-25 13:03:55 -0400

    add utils and function

--- passwordgenerator/src/App.js
+++ passwordgenerator/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import GenratePassword from './utils/GeneratePassword';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      length: 8,
+      newLength: 8,
+      pwd: "",
+      upperCase: true,
+      lowerCase: true,
+      numeric: true,
+      symbol: false,
+      typing: false,
+      typingTimeout: 0
+    }
+  }
+
+  componentDidMount() {
+    this.generatePwd();
+  }
+  generatePwd() {
+    const { upperCase, lowerCase, numeric, symbol, length } = this.state;
+    let pwd = new RandomPassword()
+      .setLength(length)
+      .setLowerCase(lowerCase)
+      .setUpperCase(upperCase)
+      .setNumberCase(numeric)
+      .setSymbol(symbol)
+      .generate();
+    this.setState({ pwd });
+  }
+
+  handleCheckbox(e) {
+    const { name, checked } = e.target;
+    this.setState({
+      [name]: checked
+    });
+    this.generatePwd();
+  }
+
+  handleLenghtChange({ target: { value } }) {
+    if (value >= 40) {
+      value = 40;
+    }
+    this.setState({ length: value }, () => this.generatePwd());
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
