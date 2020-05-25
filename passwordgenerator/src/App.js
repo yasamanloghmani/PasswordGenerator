61d7dfedcb881798cf0de40f2d548ea99648f783
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-05-25 16:38:54 -0400

    Finished

--- passwordgenerator/src/App.js
+++ passwordgenerator/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import GenratePassword from './utils/GeneratePassword';
+import { GenratePassword } from './utils/GeneratePassword';
 import './App.css';
 
 class App extends Component {
@@ -23,7 +23,7 @@ class App extends Component {
   }
   generatePwd() {
     const { upperCase, lowerCase, numeric, symbol, length } = this.state;
-    let pwd = new RandomPassword()
+    let pwd = new GenratePassword()
       .setLength(length)
       .setLowerCase(lowerCase)
       .setUpperCase(upperCase)
@@ -51,7 +51,141 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div>
+        <div className="container" style={{ marginTop: 20 }}>
+          <section>
+            <header>
+              <div className="row">
+                <div className="col">
+                  <h1 className="main-header">
+                    Generate a secure password
+                  </h1>
+                </div>
+              </div>
+            </header>
+            <div className="input-container">
+              <input
+                id="input"
+                name="password"
+                type="text"
+                readOnly
+                value={this.state.pwd}
+              />
+            </div>
+          </section>
+          {/* <hr /> */}
+          <section>
+            <header>
+              <h3>Customize your password</h3>
+            </header>
+            {/* <fieldset> */}
+              <div className="row">
+                <div className="col">
+                  <div className="form-group">
+                  <div className="checkbox-container1">
+                    <label className="checkbox-container switch">
+                      Uppercase
+                      <input
+                        type="checkbox"
+                        checked={this.state.upperCase}
+                        name="upperCase"
+                        onChange={e => this.handleCheckbox(e)}
+                      />
+                      <span className="slider round" />
+                    </label>
+                  </div>
+                    <div className="checkbox-container1">
+                    <label className="checkbox-container switch">
+                      Lowercase
+                      <input
+                        type="checkbox"
+                        checked={this.state.lowerCase}
+                        name="lowerCase"
+                        onChange={e => this.handleCheckbox(e)}
+                      />
+                      <span className="slider round" />
+                    </label>
+                    </div>
+                    <div className="checkbox-container1">
+                    <label className="checkbox-container switch">
+                      Numeric
+                      <input
+                        type="checkbox"
+                        checked={this.state.numeric}
+                        name="numeric"
+                        onChange={e => this.handleCheckbox(e)}
+                      />
+                      <span className="slider round" />
+                    </label>
+                    </div>
+                    </div>
+                    <div className="checkbox-container1">
+                    <label className="checkbox-container switch">
+                      Symbols
+                      <input
+                        type="checkbox"
+                        checked={this.state.symbol}
+                        name="symbol"
+                        onChange={e => this.handleCheckbox(e)}
+                      />
+                      <span className="slider round" />
+                    </label>
+                    </div>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="form-group">
+                    <div className="row length-input">
+                      <div className="col">
+                        <label htmlFor="email">Password Length:</label>
+                        &nbsp;&nbsp;
+                      </div>
+                      <div className="col lngh">
+                        <input
+                          type="number"
+                          min="8"
+                          max="40"
+                          style={{ width: 65 }}
+                          value={this.state.length}
+                          onChange={e => this.handleLenghtChange(e)}
+                        />
+                      </div>
+                    </div>
+                    &nbsp;
+                    <div className="slider-container">
+                      <input
+                        className="hi"
+                        type="range"
+                        min="8"
+                        max="40"
+                        value={this.state.length}
+                        onChange={e => this.handleLenghtChange(e)}
+                      />
+                    </div>
+                  </div>
+                </div>
+            {/* </fieldset> */}
+            <br />
+            <div>
+              <div className="row">
+                <div className="col">
+                  <button
+                    className="btn  btn-primary"
+                    onClick={() => {
+                      this.generatePwd();
+                    }}
+                  >
+                    Generate
+                  </button>
+                </div>
+              </div>
+
+              <br />
+              <br />
+            </div>
+          </section>
+        </div>
+      </div>
       </div>
     );
   }
